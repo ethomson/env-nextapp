1e879cb5ef1f500386830ea566e35aced10422c1
Edward Thomson <ethomson@edwardthomson.com>
2024-05-23 15:54:18 +0100

    headers

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,34 @@
 'use server';
 
-import Image from "next/image";
+import Image from 'next/image';
+import { headers } from 'next/headers'
 
 export default async function Home() {
+  const request: { [key: string]: string; } = { };
+
+  headers().forEach((value, key) => {
+    request[key] = value;
+  })
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+    <main className="min-h-screen items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm">
+        <h2 className="text-xl font-bold pb-5">Request Headers:</h2>
+
+        <div>
+          {Object.keys(request).sort().map((key, i) => {
+            return (
+              <div key={key}>
+                <b>{key}</b> = {request[key]}
+              </div>
+            );
+          })}
+        </div>
+
+        <hr className="mt-10 mb-5 h-1 bg-black border-0 rounded" />
+
+        <h2 className="text-xl font-bold pt-5 pb-5">Environment Variables:</h2>
+
         <div>
           {Object.keys(process.env).sort().map((key, i) => {
             return (
@@ -13,7 +36,6 @@ export default async function Home() {
                 <b>{key}</b> = {process.env[key]}
               </div>
             );
-            console.log(key);
           })}
         </div>
       </div>
